e0eb025124ed86a63d222507212ec34cf53bc9d9
najah <najahaboobacker79@gmail.com>
2024-02-17 19:04:24 +0530

    najah commit

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -9,11 +9,14 @@ class FoodPageBody extends StatefulWidget {
 //carosal added
 
 class _FoodPageBodyState extends State<FoodPageBody> {
+  PageController pageController=PageController(viewportFraction: 0.85);
   @override
   Widget build(BuildContext context) {
     return Container(
+      color: Colors.redAccent,
       height: 320,
       child: PageView.builder(
+        controller: pageController,
         itemCount: 5,
         itemBuilder: (context, position) {
           return _buildPageItem(position);
@@ -23,13 +26,35 @@ class _FoodPageBodyState extends State<FoodPageBody> {
   }
 
   Widget _buildPageItem(int index) {
-    return Container(
-      height: 220,
-      margin: EdgeInsets.only(left: 5, right: 5),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(30),
-        color: const Color(0xFF69c5df),
+    return Stack(
+      children:[ Container(
+        height: 250,
+        margin: EdgeInsets.only(left: 5, right: 5),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(30),
+          color:index.isEven?Color(0xFF69c5df):Color(0xFF9294cc),
+          
+       image: DecorationImage(
+            fit: BoxFit.cover,
+            image: AssetImage("assets/image/food1.jfif"),),),
       ),
-    );
+      Align(
+        child: Container(
+          alignment: Alignment.bottomCenter,
+          height: 150,
+          margin: EdgeInsets.only(left: 30, right: 30,bottom: 15),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30),
+            color:Colors.white,
+),
+child: Container( 
+  padding: EdgeInsets.only(top: 10,left: 15,right: 15),
+child: Column(
+  children: [],
+),
+),
+        ),
+      ),
+    ],);
   }
 }
